5afebfaba9e65588490e16297882f913c1d85bfc
wilfex <wilfex81@gmail.com>
2022-04-28 13:41:29 +0300

    Fixed an error in models

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Communities, Courses, Users
 
-admin.site.register(Communities, 
-                    Users,
-                    Courses)
+admin.site.register(Communities),
+admin.site.register(Courses),
+admin.site.register(Users)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
-from email.policy import default
-from pyexpat import model
+
 from django.db import models
 
 # Create your models here.
@@ -17,60 +16,33 @@ class Users(models.Model):
 class Courses(models.Model):
     
     COURSE_DETAILS = [
-        ('Difficulty levels'(
-                'Basic',(
+        ('Difficulty levels',(
                     ('easy', 'Easy'),
-                    ('medium', 'Medium'),
+                    ('medium', 'Intermediate'),
                     ('hard', 'Hard'),
-                ),
-                'Intermediate',(
-                    ('easy', 'Easy'),
-                    ('medium', 'Medium'),
-                    ('hard', 'Hard'),
-                ),
-                'Hard',(
-                    ('easy', 'Easy'),
-                    ('medium', 'Medium'),
-                    ('hard', 'Hard'),
-                ),
-            
                 )
             
-        )
+        ),
         
-        ('Course Type '(
+        ('Course Type ',(
                 ('instructor-led', 'Instructor-Led'),
                 ('on-demand', 'On-Demand'),
                 ('both', 'Both'),
             )
             
-        )
-        
-        ('Lessons'(
-                'Lesson 1',(
-                    ('lecturer_one', 'Lecturer_One'),
-                    ('lecturer_two', 'Lecturer_Two'),
-                    ('lecturer_three', 'Lecturer_Three'),
-                ),
-                'Lesson 2',(
-                    ('lecturer_one', 'Lecturer_One'),
-                    ('lecturer_two', 'Lecturer_Two'),
-                    ('lecturer_three', 'Lecturer_Three'),
-                ),
-                'Lesson 3',(
-                    ('lecturer_one', 'Lecturer_One'),
-                    ('lecturer_two', 'Lecturer_Two'),
-                    ('lecturer_three', 'Lecturer_Three'),
-                ),
-            
+        ),
+        ('Lessons',(
+                    ('1', 'Lesson one'),
+                    ('2', 'Lesson two'),
+                    ('3', 'Lesson three'),
                 )
             
-        )
+        ),
         
-        ('Practical Assessment '(
+        ('Practical Assessment',(
                 ('assessment_one', 'Assessment-One'),   
             )   
-        )
+        ),
         
         
         
@@ -78,30 +50,21 @@ class Courses(models.Model):
     
     
     SPECIALTY = [
-        ('Programming Languages'(
+        ('Programming Languages',(
                     ('java', 'Java'),
                     ('python', 'Python'),
                     ('javascript', 'JavaScript'),
-                ),
                 )
+                ),
         
-        ('Frameworks'(
-                'Java',(
-                    ('spring', 'Spring'),
-                    ('hibernate', 'Hibernate'),
-                    ('struts', 'Struts'),
+        ('Frameworks',(
+                    ('java', 'Spring'),
+                    ('python', 'Django'),
+                    ('javascript', 'ReactJs'),
+                )
+               
                 ),
-                'Python',(
-                    ('flask', 'Flask'),
-                    ('django', 'Django'),
-                ),
-                'Javascript',(
-                    ('reactjs', 'ReactJs'),
-                    ('vuejs', 'VueJs'),
-                    ('nodejs', 'NodeJs'),
-                ),
-                ),
-        )
+        
     ]
     
     title = models.CharField(max_length=100)
@@ -117,19 +80,19 @@ class Courses(models.Model):
     price = models.IntegerField(default = 0)
         
 
-    def __str__(self) -> str:
-        return super().__str__()
+    def __str__(self):
+        return self.title
 
 
 class Communities(models.Model):
     
     CHAMPIONS_LIST = [
         
-        'Champions'(
+        'Champions',(
             ('champion1', 'Champion1'),
             ('champion2', 'Champion2'),
             ('champion3', 'Champion3'),
-        )
+        ),
     ]
     
     
@@ -137,7 +100,7 @@ class Communities(models.Model):
     champions = models.CharField(max_length=50,
                                  choices = CHAMPIONS_LIST,
                                  default=0),
-    upvotes = models.IntegerChoices(default=0)
+    upvotes = models.IntegerField(default=0)
     
 
     def __str__(self) -> str:
