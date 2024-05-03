7e169bee8a50a96e316c2f4ba7417dc06419bfa1
Wilfex <84175893+wilfex81@users.noreply.github.com>
2024-05-03 06:57:27 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2no(o*))pnf!hpgwh7xv#sffjhy4tgaje3*6&8a8^(u6*qzip7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['code-with-flex-backend.onrender.com', 'localhost', '127.0.0.1']
 
